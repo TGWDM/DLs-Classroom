97b57447969453244ae1ef65103e45eb6be08016
TGWDM <tyrellgws99@gmail.com>
2026-08-14 21:20:56 +0100

    Setup backend to use net 10 EF

--- backend/dlClass.API/obj/Debug/net10.0/dlClass.API.AssemblyInfo.cs
+++ backend/dlClass.API/obj/Debug/net10.0/dlClass.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33cfca90853dfe0273caeda84be31e553c5e525a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730206c89dff6235e8db196758e79c33b5b649a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
