d6c8290813f216d72f148ab2e905b805569ba935
TGWDM <tyrellgws99@gmail.com>
2026-08-26 20:44:01 +0100

    Added github actions

--- backend/dlClass.API/obj/Debug/net10.0/dlClass.API.AssemblyInfo.cs
+++ backend/dlClass.API/obj/Debug/net10.0/dlClass.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12427289e48aa5e875ac10cdc568d6b2735e982c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a24e9e7f6aac8c10d73a0c8a1bfd9701b82f336")]
 [assembly: System.Reflection.AssemblyProductAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dlClass.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
