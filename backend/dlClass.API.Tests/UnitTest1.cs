ea6ec478ce81f6d1865d436b45db15aea564a80d
TGWDM <tyrellgws99@gmail.com>
2026-09-01 20:40:24 +0100

    Added sample test for backend

--- backend/dlClass.API.Tests/UnitTest1.cs
+++ backend/dlClass.API.Tests/UnitTest1.cs
@@ -1,10 +1,12 @@
-﻿namespace dlClass.API.Tests;
+﻿using Xunit;
+
+namespace dlClass.API.Tests;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        Assert.True(true);
     }
 }
